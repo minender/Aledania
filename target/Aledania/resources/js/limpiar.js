d66087e790265a4478b83af6bdf09bc1800a69ff
Federico Flaviani <federico@correlaciones.ldc.usb.ve>
2021-02-16 19:59:15 -0500

    probando gitignore otra vez

--- target/Aledania/resources/js/limpiar.js
+++
@@ -1,9 +0,0 @@
-function limpiar()
-{
-       var texArea=document.getElementById('termino_string');
-       if(texArea.value != "")
-       {
-             if(confirm("Sure you want to delete the content of the text area?"))
-                 texArea.value="";
-       }
-}
